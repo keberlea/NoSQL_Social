be79e0800d4df680b2bacf20a41acd6ad3c3781d
Alicia Keberle <122774607+keberlea@users.noreply.github.com>
2023-06-08 20:35:38 -0700

    made changes to controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,33 +26,30 @@ module.exports = {
 
     //create user
     createUser(req, res) {
-        User.create(req.body)({
-            username: req.body.username,
-            email: req.body.email,
+      User.create(req.body)(
+        {
+          username: req.body.username,
+          email: req.body.email,
         })
-            .then((dbUserData) => res.json(dbUserData))
-            .catch((err) => res.status(400).json(err));
+        .then((dbUserData) => res.json(dbUserData))
+        .catch((err) => res.status(400).json(err));
     },
 
     //update user
-    updateUser(req, res) {
+    updateUser (req, res) {
         User.findOneAndUpdate(
             { _id: req.params.userId }, 
             {
               username: req.body.username,
               email: req.body.email
             }, 
-            { new: true }, 
-            (err, result) => {
-              if (result) {
-                res.status(200).json(result);
-                console.log(`Updated: ${result}`);
-              } else {
-                console.log(err);
-                res.status(500).json({ message: 'error', err });
-              }
-            }
-        )
+            { new: true })
+            .then((user) =>
+              !user
+              ? res.status(404).json({ message: 'No user found with this id!' })
+              : res.json(user)
+            ) 
+            .catch((err) => res.status(400).json(err));
     },
 
     // delete user
